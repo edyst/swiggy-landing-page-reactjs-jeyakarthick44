e0e8447397d136b17b0788fd174960e030b72534
jeyakarthick44 <jeyakarthick408@gmail.com>
2021-11-30 19:56:52 +0530

    finally completed

--- swiggy/src/App.js
+++ swiggy/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react'
+import Herosection from './components/Herosection/hero'
+import Features from './components/Features/features'
+import Restaurants from './components/Restaurants/restaurants'
+import Footer from './components/Footer/footer'
+
 import './App.css';
+import './components/FontAwesome'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <Herosection />
+      <Features />
+      <Restaurants />
+      <Footer />
     </div>
-  );
+  )
 }
 
 export default App;
